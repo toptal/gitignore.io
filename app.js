3e90d9b64fb347946b1d515221a0254190ec7d1f
Joe Blau <josephblau@gmail.com>
2013-02-15 08:39:32 -0800

    Added favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
