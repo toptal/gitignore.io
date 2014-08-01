4b021f7ffd832c4b8930c24bde3780ae025cb4e6
Joe Blau <josephblau@gmail.com>
2014-08-01 16:48:07 -0700

    Fixed count bug

--- routes/web.js
+++ routes/web.js
@@ -10,7 +10,7 @@ var app = require('../app')
 exports.index = function(req, res){
   res.setHeader('Cache-Control', 'public, max-age=' + (app.oneDayCache / 1000));
   res.setHeader('Expires', new Date(Date.now() + app.oneDayCache).toUTCString());
-  res.render('index', { title: 'gitignore.io - Create useful .gitignore files for your project', fileCount: app.gitIgnoreFileCount});
+  res.render('index', { title: 'gitignore.io - Create useful .gitignore files for your project', fileCount: datastore.fileCount});
 };
 
 /*
