87038d7843b1826412871a60c2212f5266503077
Joe Blau <jblau@uberatc.com>
2016-12-21 16:20:02 -0800

    Updated package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GitignoreIO",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
     ],
     exclude: [
         "Config",
@@ -11,5 +11,7 @@ let package = Package(
         "Public",
         "Resources",
         "Tests",
+        "data",
+        "wiki"
     ]
 )
