0c968738b0231c2ee07398bc7f22045637d42b45
Joe Blau <josephblau@gmail.com>
2017-01-28 18:27:26 -0500

    Removed unnecessar cruft added by Bower.js (#280)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "App", dependencies: ["GitignoreIOServer"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4)
     ],
     exclude: [
         "Config",
