bbe4b14401128fb32f842a07a4fd603d7ba33382
Joe Blau <josephblau@gmail.com>
2017-04-29 22:35:38 -0400

    Added stacks

--- Sources/GitignoreIOServer/Enum/TemplateSuffix.swift
+++ Sources/GitignoreIOServer/Enum/TemplateSuffix.swift
@@ -10,12 +10,13 @@ import Foundation
 
 /// Template Suffix Enum
 internal enum TemplateSuffix {
-    case template, patch
+    case template, patch, stack
     
     internal var `extension`: String {
         switch self {
         case .template: return "gitignore"
         case .patch: return "patch"
+        case .stack: return "stack"
         }
     }
     
@@ -23,6 +24,7 @@ internal enum TemplateSuffix {
         switch self {
         case .template: return "\n### \(name) ###\n"
         case .patch: return "\n### \(name) Patch ###\n"
+        case .stack: return "\n### \(name) Stack ###\n"
         }
     }
 }
