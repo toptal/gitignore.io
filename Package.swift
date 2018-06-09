c7e42d2780a3d8ed50092c11caae40795b44c2c7
Joe Blau <jblau@uberatc.com>
2018-06-09 12:45:20 -0700

    Xcode project opening

--- Package.swift
+++ Package.swift
@@ -1,19 +1,13 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "GitignoreIO",
-    targets: [
-        Target(name: "App", dependencies: ["GitignoreIOServer"]),
-    ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
-    exclude: [
-        "Config",
-        "Localization",
-        "Public",
-        "Resources",
-        "data",
-        "wiki"
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "GitignoreIOServer", dependencies: ["App"])
     ]
 )
