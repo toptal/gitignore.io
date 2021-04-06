c8d9b2f4c6b0b61105a64bff0b8a1645911834fe
Konstantin Rybakov <i@krybakov.com>
2021-04-06 18:04:47 +0300

    Upading templates from https://github.com/toptal/gitignore

--- .travis/update-submodule.sh
+++ .travis/update-submodule.sh
@@ -9,7 +9,6 @@ if [[ `git status --porcelain` ]]; then
   echo "status: Updating templates"
   git add .
   git commit -m "Upading templates from https://github.com/toptal/gitignore"
-  git push origin master
 else
   echo "status: No updates"
 fi
