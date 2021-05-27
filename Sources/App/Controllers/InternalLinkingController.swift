2d5baac20546f4e1ab556bd86fbae1c4333b2678
Maciek Caputa <8134011+macap@users.noreply.github.com>
2021-05-27 13:05:26 +0200

    Cleanup internalLinkingController

--- Sources/App/Controllers/InternalLinkingController.swift
+++ Sources/App/Controllers/InternalLinkingController.swift
@@ -9,18 +9,17 @@ internal struct InternalLinkingController: ReadOnlyInternalLinkingProtocol {
      internal var links = InternalLinks(links: [InternalLink]())
      init() {
         let directory = DirectoryConfig.detect()
+        let internalLinksFilePath = URL(fileURLWithPath: directory.workDir)
+                .appendingPathComponent("Resources", isDirectory: true)
+                .appendingPathComponent("links.json", isDirectory: false)
         var rawLinks = [InternalLink]()
         do {
-          let jsonData = try Data(contentsOf: URL(fileURLWithPath: directory.workDir)
-                .appendingPathComponent("Resources", isDirectory: true)
-                .appendingPathComponent("links.json", isDirectory: false))
-
-            rawLinks = try JSONDecoder().decode([InternalLink].self, from: jsonData)
-              
-
-            self.links = InternalLinks(links: rawLinks)
+          let jsonData = try Data(contentsOf: internalLinksFilePath)
+          rawLinks = try JSONDecoder().decode([InternalLink].self, from: jsonData)
+        
+          self.links = InternalLinks(links: rawLinks)
         } catch {
-            print("‼️ You might not have done a recursive clone to update your submodules:\n‼️ `git submodule update --init --recursive`")
+            print("‼️ Could not load internal links from json, check:\n‼️ Resources/links.json")
         }
     }
 }
