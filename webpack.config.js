63b88dc666dcf1c8865794069e61edaf6719cb5a
Filipe Chagas <filipe.chagas@toptal.com>
2021-06-30 09:30:42 -0300

    [TRA-1723] Move all assets to webpack (#541)

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
   optimization: {
     minimizer: [
       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
+     `...`,
       new CssMinimizerPlugin(),
     ],
   },
